8d3f278837c00770a56f1a6d7908188f9cc12ca7
MTuglaci <tuglacimehmet@gmail.com>
2020-11-21 18:45:36 +0300

    Project initialization

---
+++ src/test/java/com/memos/backend/MemosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
